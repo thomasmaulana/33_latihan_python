a159ee7247a069326258c3bc377a82e64100b297
thomasmaulana <33.thomas.tkjsmeksa25@gmail.com>
2026-08-02 20:23:28 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+angka = int(input("1:"))
+angka2 = int(input("2:"))
+if angka % 2 == 0:
+    print(f"angka {angka} adalah bilangan genap")
+else :
+    print(f"angka {angka} adalah bilangan ganjil")
+if angka2 % 2 == 0:
+    print(f"angka2 {angka2} adalah bilangan genap")
+else :
+    print(f"angka2 {angka2} adalah bilangan ganjil")
