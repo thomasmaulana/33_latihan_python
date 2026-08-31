0597bf257e19ce3b2a333e536e136171b076ea58
thomasmaulana <33.thomas.tkjsmeksa25@gmail.com>
2026-08-30 19:30:06 -0700

    while true, try, except

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-angka = int(input("1:"))
-angka2 = int(input("2:"))
-if angka % 2 == 0:
-    print(f"angka {angka} adalah bilangan genap")
-else :
-    print(f"angka {angka} adalah bilangan ganjil")
-if angka2 % 2 == 0:
-    print(f"angka2 {angka2} adalah bilangan genap")
-else :
-    print(f"angka2 {angka2} adalah bilangan ganjil")
+while True:
+    input_1 = input("1 (atau ketik 'q' untuk keluar): ")
+    if input_1.lower() == 'q':
+        print("program selesai. Terima kasih!")
+        break
+
+    try:
+        angka = int(input_1)
+        angka2 = int(input("2: "))
+    except ValueError:
+        print("masukkan angka yang valid!\n")
+        continue
+
+    if angka % 2 == 0:
+        print(f"angka {angka} adalah bilangan genap")
+    else :
+        print(f"angka {angka} adalah bilangan ganjil")
+    if angka2 % 2 == 0:
+        print(f"angka2 {angka2} adalah bilangan genap")
+    else :
+        print(f"angka2 {angka2} adalah bilangan ganjil")
+print("_" * 30)
